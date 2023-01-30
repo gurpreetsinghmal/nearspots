2dde8314f20fc9b544f5a69f9a1bed36098e15ee
Technominds <harpreetsinghm786@gmail.com>
2023-01-30 09:34:04 +0530

    m

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,7 +35,7 @@
             <x-listings />
 
             <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg ">
-                hi
+                hello
             </div>
         </div>
     </div>
