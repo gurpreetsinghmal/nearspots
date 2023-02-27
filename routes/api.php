71f5b40af7d705b643474dfa71453b7cdede32ac
Technominds <harpreetsinghm786@gmail.com>
2023-02-27 16:01:34 +0530

    ok

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('shop/save1',[SaveshopController::class,'save']);
+Route::post('shop/save',[SaveshopController::class,'save']);
